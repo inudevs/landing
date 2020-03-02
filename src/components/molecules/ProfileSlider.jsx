fda0e00980e36dcd3214d6d540d46192a223bccd
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2020-03-02 21:05:50 +0900

    feat: add notranslate meta tag and format ProfileSlider

--- src/components/molecules/ProfileSlider.jsx
+++ src/components/molecules/ProfileSlider.jsx
@@ -1,78 +1,77 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { CircleSlider } from "react-circle-slider";
 import styled from 'styled-components';
 
 class ProfileSlider extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: 0,
-            Isstart: 0,
-            Isend: 0,
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0,
+      Isstart: 0,
+      Isend: 0,
 
-            addvalue: 0.7
-        };
-    }
-
-    handleChange = value => {
-        console.log(`Changed value ${value}`);
-        this.setState({ value });
+      addvalue: 0.7
     };
+  }
 
-    handleChangeRange = event => {
-        this.setState({
-            value: event.target.valueAsNumber,
-        });
-    };
+  handleChange = value => {
+    console.log(`Changed value ${value}`);
+    this.setState({ value });
+  };
 
-    Anim(e) {
-        setTimeout(function () {
-            this.setState({ Isstart: this.state.Isstart + e, value: this.state.Isstart });
-        }.bind(this), 10)
-        if (this.state.Isstart >= 99) {
-            this.setState({
-                Isend: 1
-            });
+  handleChangeRange = event => {
+    this.setState({
+      value: event.target.valueAsNumber,
+    });
+  };
+
+  Anim(e) {
+    setTimeout(function () {
+      this.setState({ Isstart: this.state.Isstart + e, value: this.state.Isstart });
+    }.bind(this), 10)
+    if (this.state.Isstart >= 99) {
+      this.setState({
+        Isend: 1
+      });
+    }
+  }
+
+  Sliderspeed = () => {
+    if (this.state.Isstart <= 50) {
+      return (
+        0.7 + (0.7 / 50 * this.state.Isstart)
+      )
+    } else if (50 <= this.state.Isstart <= 100) {
+      return (
+        1.4 - (0.7 / 75 * this.state.Isstart)
+      )
+    }
+  }
+
+  render() {
+    const { value, Isstart, Isend } = this.state;
+    return (
+      <>
+        {
+          Isend === 0
+            ?
+            <Logoslider>
+              <CircleSlider value={value} onChange={this.handleChange} gradientColorTo="#F99F4D" gradientColorFrom="#C32E92" knobRadius="0" circleWidth="0" progressWidth="24" size="248" />
+              {
+                Isstart < 100
+                  ? <>{this.Anim(this.Sliderspeed())}</>
+                  : <></>
+              }
+            </Logoslider>
+            : <Logoslider>
+              <FadeOut>
+                <CircleSlider value={100} onChange={this.handleChange} gradientColorTo="#F99F4D" gradientColorFrom="#C32E92" knobRadius="0" circleWidth="0" progressWidth="24" size="248" />
+              </FadeOut>
+            </Logoslider>
         }
-    }
-
-    Sliderspeed = () => {
-        if (this.state.Isstart <= 50) {
-            return (
-                0.7 + (0.7 / 50 * this.state.Isstart)
-            )
-        } else if (50 <= this.state.Isstart <= 100) {
-            return (
-                1.4 - (0.7 / 75 * this.state.Isstart)
-            )
-        }
-    }
-
-    render() {
-        const { value, Isstart, Isend } = this.state;
-        return (
-            <>
-                {
-                    Isend === 0
-                        ?
-                        <Logoslider>
-                            <CircleSlider value={value} onChange={this.handleChange} gradientColorTo="#F99F4D" gradientColorFrom="#C32E92" knobRadius="0" circleWidth="0" progressWidth="24" size="248" />
-                            {
-                                Isstart < 100
-                                    ? <>{this.Anim(this.Sliderspeed())}</>
-                                    : <></>
-                            }
-                        </Logoslider>
-                        : <Logoslider>
-                            <FadeOut>
-                                <CircleSlider value={100} onChange={this.handleChange} gradientColorTo="#F99F4D" gradientColorFrom="#C32E92" knobRadius="0" circleWidth="0" progressWidth="24" size="248" />
-                            </FadeOut>
-                        </Logoslider>
-                }
-            </>
-        );
-    }
+      </>
+    );
+  }
 }
 
 const Logoslider = styled.div`
