725f294d4e5230dca15218386abdf47a864f7f87
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2020-03-15 01:10:41 +0900

    chore: update profile image

--- src/pages/Members.jsx
+++ src/pages/Members.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
 import Page from '../components/atoms/Page';
+import PageSection from '../components/organisms/PageSection';
+import MemberList from '../components/templates/MemberList';
 import Header from '../components/templates/Header';
 
 const MembersPage = () => {
   return (
     <Page>
       <Header />
+      <PageSection
+        title="동아리를 이루는 사람들이에요."
+      >
+        <MemberList />
+      </PageSection>
     </Page>
   );
 };
