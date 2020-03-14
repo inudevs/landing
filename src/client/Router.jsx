790de570cffcf99e3637e599bc3072888d01ce13
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2020-03-14 22:13:14 +0900

    feat: add routes

--- src/client/Router.jsx
+++ src/client/Router.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from '../pages/Home';
-import Index from '../pages/Index';
+import Story from '../pages/Story';
+import Members from '../pages/Members';
+import Projects from '../pages/Projects';
+import Contents from '../pages/Contents';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/old" component={Index} />
+        <Route exact path="/story" component={Story} />
+        <Route exact path="/members" component={Members} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/contents" component={Contents} />
       </Switch>
     </BrowserRouter>
   );
