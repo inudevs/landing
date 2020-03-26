28a67fb89c1c47281ed4a1a6ac8949dd4f697a61
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2020-03-26 20:37:15 +0900

    feat: update members and place tape again

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,15 @@ import Router from './client/Router';
 
 import Global from './components/atoms/Global';
 import Footer from './components/molecules/Footer';
-// import Text from './components/atoms/Text';
-// import LayeredText from './components/molecules/LayeredText';
+import Text from './components/atoms/Text';
+import LayeredText from './components/molecules/LayeredText';
 
 function App() {
   return (
     <>
       <Global />
       <Container>
-        {/* <Cover>
+        <Cover>
           <Tape>LANDING AT<br />2020-03-26</Tape>
           <Desc>
             많은 기대 부탁드립니다.
@@ -21,7 +21,7 @@ function App() {
           <Bottom>
             @inudevs
           </Bottom>
-        </Cover> */}
+        </Cover>
 
         <Router />
         <Footer />
@@ -38,38 +38,38 @@ const Container = styled.div`
   position: relative;
 `;
 
-// const Cover = styled.div`
-//   position: absolute;
-//   top: 0;
-//   bottom: 0;
-//   right: 0;
-//   left: 0;
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-//   justify-content: center;
-//   z-index: 990;
-//   background-color: rgba(0, 0, 0, 0.75);
-// `;
+const Cover = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  z-index: 990;
+  background-color: rgba(0, 0, 0, 0.75);
+`;
 
-// const Tape = styled(Text)`
-//   font-size: 85px;
-//   font-weight: 900;
-//   line-height: 1;
-//   color: white;
-//   background-color: #111;
-//   padding: 0.8rem;
-//   z-index: 999;
-// `;
+const Tape = styled(Text)`
+  font-size: 85px;
+  font-weight: 900;
+  line-height: 1;
+  color: white;
+  background-color: #111;
+  padding: 0.8rem;
+  z-index: 999;
+`;
 
-// const Desc = styled(Text)`
-//   color: white;
-//   font-size: 1.8rem;
-//   font-weight: 900;
-// `;
+const Desc = styled(Text)`
+  color: white;
+  font-size: 1.8rem;
+  font-weight: 900;
+`;
 
-// const Bottom = styled(Text)`
-//   color: white;
-//   font-size: 1.1rem;
-//   font-weight: 900;
-// `;
+const Bottom = styled(Text)`
+  color: white;
+  font-size: 1.1rem;
+  font-weight: 900;
+`;
